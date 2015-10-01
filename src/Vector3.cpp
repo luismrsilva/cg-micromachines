9d9d2e69d9278d93d17db5ad5d2f58b0b5e5d0ee
Luís Silva <luis.m.ribeiro.silva@tecnico.ulisboa.pt>
2015-10-01 09:44:05 +0100

    Add Vector3 class

---
+++ src/Vector3.cpp
@@ -0,0 +1,63 @@
+/*
+ * Vector3.cpp
+ * 
+ * */
+ 
+#include "Vector3.hpp"
+
+
+Vector3::Vector3(){
+	_x = _y = _z = 0;
+}
+
+Vector3::Vector3(double x, double y, double z){
+	_x = x;
+	_y = y;
+	_z = z;
+}
+
+Vector3::~Vector3(){
+	
+}
+
+double Vector3::getX(){
+	return _x;
+}
+
+double Vector3::getY(){
+	return _y;
+}
+
+double Vector3::getZ(){
+	return _z;
+}
+
+void Vector3::set(double x, double y, double z){
+	_x = x;
+	_y = y;
+	_z = z;
+}
+
+Vector3 Vector3::operator=(const Vector3 vec){
+	_x = vec._x;
+	_y = vec._y;
+	_z = vec._z;
+}
+
+Vector3 Vector3::operator*(double num){
+	_x *= num;
+	_y *= num;
+	_z *= num;
+}
+
+Vector3 Vector3::operator+(const Vector3 vec){
+	_x += vec._x;
+	_y += vec._y;
+	_z += vec._z;
+}
+
+Vector3 Vector3::operator-(const Vector3 vec){
+	_x -= vec._x;
+	_y -= vec._y;
+	_z -= vec._z;
+}
---
+++ src/Vector3.hpp
@@ -0,0 +1,29 @@
+/*
+ * Vector3.h
+ * 
+ * */
+
+#ifndef __VECTOR3_H
+#define __VECTOR3_H
+
+class Vector3 {
+	private:
+		double _x;
+		double _y;
+		double _z;
+		
+	public:
+		Vector3();
+		Vector3(double x, double y, double z);
+		~Vector3();
+		double getX();
+		double getY();
+		double getZ();
+		void set(double x, double y, double z);
+		Vector3 operator=(const Vector3 vec);
+		Vector3 operator*(double num);
+		Vector3 operator+(const Vector3 vec);
+		Vector3 operator-(const Vector3 vec);
+};
+
+#endif
