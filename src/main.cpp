bb1765d2b8d118fb52123fce58bfc6836f30496c
zettca <corruption1337@gmail.com>
2015-10-08 09:08:09 +0100

    add Arrow Key Bindings

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "GameManager.hpp"
 
 GameManager *gameManager;
+bool isKeyPressed[] = {false, false, false, false};
 
 void myReshape(GLsizei w, GLsizei h){
 	gameManager->reshape(w, h);
@@ -13,8 +14,27 @@ void myDisplay(void){
 	gameManager->display();
 }
 
+void turnKeyPressed(int key, bool status){
+	switch (key){
+		case GLUT_KEY_UP:			isKeyPressed[0] = status; break;
+		case GLUT_KEY_DOWN: 	isKeyPressed[1] = status; break;
+		case GLUT_KEY_LEFT: 	isKeyPressed[2] = status; break;
+		case GLUT_KEY_RIGHT: 	isKeyPressed[3] = status; break;
+	}
+}
+
+void myKeySpecial(int key, int x, int y){
+	std::cout << "called keyspecial: +" << key << std::endl;
+	turnKeyPressed(key , true);
+}
+
+void myKeyUpSpecial(int key, int x, int y){
+	std::cout << "called keyspecial: -" << key << std::endl;
+	turnKeyPressed(key , false);
+}
+
 int main(int argc, char *argv[]){
-	
+
 	glutInit(&argc, argv);
 
 	gameManager = new GameManager();
@@ -22,6 +42,10 @@ int main(int argc, char *argv[]){
 
 	glutDisplayFunc(myDisplay);
 	glutReshapeFunc(myReshape);
+
+	glutSpecialFunc(myKeySpecial);
+	glutSpecialUpFunc(myKeyUpSpecial);
+
 	glutMainLoop();
 
 	delete gameManager;
