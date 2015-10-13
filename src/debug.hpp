d823d83fe05dc4a513de0d79a9d1153bb31956e6
Luís Silva <luis.m.ribeiro.silva@tecnico.ulisboa.pt>
2015-10-13 02:10:54 +0100

    guard debug.hpp

--- src/debug.hpp
+++ src/debug.hpp
@@ -4,12 +4,18 @@
  * One way to define it is to use -D compiler flag (-DDEBUG).
  * */
 
+#ifndef __DEBUG_HPP_INCLUDED
+#define __DEBUG_HPP_INCLUDED
+
 #ifdef DEBUG
+	#include <iostream>
 	#define D_PRINT(str) \
-		cout << __FILE__ ":" << __LINE__ << "\t" str << endl
+		std::cout << __FILE__ ":" << __LINE__ << "\t" str << std::endl
 	#define D_TRACE(str) \
-		cout << __FILE__ ":" << __LINE__ << "\t" << __FUNCTION__ str << endl
+		std::cout << __FILE__ ":" << __LINE__ << "\t" << __FUNCTION__ str << std::endl
 #else
 	#define D_PRINT(str)
 	#define D_TRACE(str)
 #endif
+
+#endif //__DEBUG_HPP_INCLUDED
