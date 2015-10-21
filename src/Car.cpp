924071a18080e506057f06687409de207b9a95ed
Bruno Henriques <bruno.s.henriques@tecnico.ulisboa.pt>
2015-10-21 23:59:47 +0100

    change car tire vertice ammount

--- src/Car.cpp
+++ src/Car.cpp
@@ -57,31 +57,31 @@ void Car::draw(){
 		glRotatef(this->_angle_deg-90., 0, 0, 1.);
 	    glScalef(2.0f, 2.0f, 2.0f);
 
-		glPushMatrix();
+		glPushMatrix();	// car body
 		    glColor3f(0.7f, 0.0f, 0.1f);
 			glScalef(1, 2, 1);
 			glutSolidCube(cm(4));
 		glPopMatrix();
 
-		glPushMatrix();
+		glPushMatrix();	// car window
 			glColor3f(0.7f, 0.6f, 1.0f);
 			glScalef(1, 1.2, 0.8); // longer and less tall
 			glTranslatef(0.0f, -0.01f, 0.0f);
 			glutSolidSphere(cm(1.5), 32, 32);
 		glPopMatrix();
 
-		glPushMatrix();
+		glPushMatrix();	// car tires
 			glRotatef(-90, 0, 1, 0);
 			glColor3f(0.06f, 0.06f, 0.06f);	// tire color
 
 			glTranslatef(0.0f, -0.02f, 0.024f);
-			glutSolidTorus(0.004f, 0.005f, 32, 64);
+			glutSolidTorus(0.004f, 0.005f, 8, 16);
 			glTranslatef(0.0f, 0.045f, 0.0f);
-			glutSolidTorus(0.004f, 0.005f, 32, 64);
+			glutSolidTorus(0.004f, 0.005f, 8, 16);
 			glTranslatef(0.0f, 0.0f, -0.048f);
-			glutSolidTorus(0.004f, 0.005f, 32, 64);
+			glutSolidTorus(0.004f, 0.005f, 8, 16);
 			glTranslatef(0.0f, -0.045f, 0.0f);
-			glutSolidTorus(0.004f, 0.005f, 32, 64);
+			glutSolidTorus(0.004f, 0.005f, 8, 16);
 		glPopMatrix();
 
     glPopMatrix();
