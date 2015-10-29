ed3b4fba327c46a666f17b7ba9ea6622be6cd2a0
Sofia Reis <sofia.reis@ist.utl.pt>
2015-10-29 00:50:50 +0000

    Verifies Intersection between boxes

--- src/Box.cpp
+++ src/Box.cpp
@@ -4,26 +4,35 @@
  * */
 
 #define _USE_MATH_DEFINES
-#include <cmath>
 #include "Vector3.hpp"
 #include "GameObject.hpp"
 #include "myunits.hpp"
-#include <iostream>
-using namespace std;
 #include <GL/glut.h>
 
 #include "debug.hpp"
-
+#include <stdbool.h>
 #include "Box.hpp"
 
 Box::Box(double left, double right, double top, double bottom) : GameObject() {
+	changeTo(left, right, top, bottom);
+}
+
+Box::Box(double width, double height, const Vector3 &center) : GameObject() {
+	changeTo(width, height, center);
+}
+
+Box::~Box(){
+
+}
+
+void Box::changeTo(double left, double right, double top, double bottom) {
 	_left = left;
 	_right = right;
 	_top = top;
 	_bottom = bottom;
 }
 
-Box::Box(double width, double height, const Vector3 &center) : GameObject() {
+void Box::changeTo(double width, double height, const Vector3 &center) {
 	setPosition(center);
 	_left = center.getX() - (width / 2);
 	_right = center.getX() + (width / 2);
@@ -31,14 +40,7 @@ Box::Box(double width, double height, const Vector3 &center) : GameObject() {
 	_bottom = center.getY() - (height / 2);
 }
 
-Box::~Box(){
-
-}
-
-void Box::draw(){
-	D_TRACE();
-
-	Vector3 *pos = this->getPosition();
+void Box::draw() const{
 	glColor3f(0.0, 0.0, 1.0);
 	glBegin(GL_LINE_LOOP);
 	glVertex3f(_left, _top, 0);
@@ -46,4 +48,29 @@ void Box::draw(){
 	glVertex3f(_right, _bottom, 0);
 	glVertex3f(_right, _top, 0);
 	glEnd();
+}
+
+bool Box::isIntersecting(const Box &box) const{
+	
+	Box *a = (Box*) this;
+	Box *b = (Box*) &box;
+	
+	if (a->_right < b->_right){
+		a = (Box*) &box;
+		b = (Box*) this;
+	}
+
+	//Verificar se ha intersecao horizontal
+	if (b->_left <= a->_left <= b->_right) {
+		if (a->_top < b->_top) {
+			a = (Box*)&box;
+			b = (Box*) this;
+		}
+		//Verificar se ha intersecao vertical
+		if (b->_bottom <= a->_bottom <= b->_top) {
+			return true;
+		}
+	}
+
+	return false;
 }
--- src/Box.hpp
+++ src/Box.hpp
@@ -8,13 +8,17 @@
 
 #include "GameObject.hpp"
 #include "Vector3.hpp"
+#include <stdbool.h>
 
 class Box : public GameObject{
 	public:
 		Box(double left, double right, double top, double bottom);
 		Box(double width, double height, const Vector3 &center);
 		~Box();
-		void draw();
+		void changeTo(double left, double right, double top, double bottom);
+		void changeTo(double width, double height, const Vector3 &center);
+		void draw() const;
+		bool isIntersecting(const Box &box) const;
 	private:
 		double _left;
 		double _right;
