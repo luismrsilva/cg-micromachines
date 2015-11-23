aee01fd137ad15650425009dfd8ec4680784d7e3
Bruno Henriques <bruno.s.henriques@tecnico.ulisboa.pt>
2015-11-23 22:59:06 +0000

    Add Texture class base

---
+++ src/Texture.cpp
@@ -0,0 +1,36 @@
+/*
+ * Texture.cpp
+ *
+ * */
+
+#include "Texture.hpp"
+#include "Vector3.hpp"
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+#include <GL/glut.h>
+
+#include "debug.hpp"
+#include "myunits.hpp"
+#include "game_config.hpp"
+
+
+Texture::Texture(){
+
+}
+
+Texture::Texture(GLfloat r, GLfloat g, GLfloat b){
+
+}
+
+Texture::~Texture(){
+}
+
+void Texture::setTexture(GLfloat r, GLfloat g, GLfloat b){
+
+}
+
+void Texture::draw(){
+
+}
